3695c97d516d82940359ca9e6bc21a2750898c39
mandelatuks <manderat206@gmail.com>
2022-05-26 01:42:01 +0200

    Add style for the calculator and exported in App js file

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,7 +19,7 @@ export class Calculator extends Component {
         <button type="button">7</button>
         <button type="button">8</button>
         <button type="button">9</button>
-        <button type="button" className="sidenumber">×</button>
+        <button type="button" className="sideNumber">×</button>
         <button type="button">4</button>
         <button type="button">5</button>
         <button type="button">6</button>
@@ -28,7 +28,7 @@ export class Calculator extends Component {
         <button type="button">2</button>
         <button type="button">3</button>
         <button type="button" className="sideNumber">+</button>
-        <button type="button">0</button>
+        <button type="button" className="Two-box">0</button>
         <button type="button">.</button>
         <button type="button" className="sideNumber">=</button>
       </div>
