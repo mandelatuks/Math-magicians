9688f10254998698473bee56a5b451c65462356d
mandelatuks <manderat206@gmail.com>
2022-05-27 12:31:20 +0200

    Add logic folder which contains the necessary code for calculation

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,25 +12,7 @@ export class Calculator extends Component {
         <div className="output">
           <div className="operand"> 0</div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="sideNumber">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="sideNumber">×</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="sideNumber">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="sideNumber">+</button>
-        <button type="button" className="Two-boxes">0</button>
-        <button type="button">.</button>
-        <button type="button" className="sideNumber">=</button>
+
       </div>
     );
   }
